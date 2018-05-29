27e50586e163de9cf80b3d5d8d8f464c0e225d57
Marc Boanas <marcboanas1@marcs-imac.home>
2018-05-29 21:34:19 +0100

    Page layout with header and off-canvas navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [
